1039ccb63c05d7c2a146b5fc4c5ec8dce1d9e6fa
Przemyslaw Pradela <przemyslaw.pradela@gmail.com>
2020-02-24 23:59:50 +0100

    Zmiana metody mapowania obiektów bazy danych z JPA na Hibernate Session

--- docsrepo-ws/src/main/java/docsrepo/ws/DocsWebService.java
+++ docsrepo-ws/src/main/java/docsrepo/ws/DocsWebService.java
@@ -1,23 +1,18 @@
 package docsrepo.ws;
 
 import docsrepo.ws.entities.Document;
+import docsrepo.ws.utils.HibernateUtil;
 import java.util.List;
 import javax.jws.Oneway;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 @WebService(serviceName = "DocsService")
 public class DocsWebService {
-
-    private final EntityManagerFactory emf;
-
-    public DocsWebService() {
-        this.emf = Persistence.createEntityManagerFactory("docsrepoPU");
-    }
 
     /**
      * Web service operation
@@ -27,11 +22,17 @@ public class DocsWebService {
     @WebMethod(operationName = "createDocument")
     @Oneway
     public void createDocument(@WebParam(name = "doc") Document doc) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(doc);
-        em.getTransaction().commit();
-        em.close();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx;
+        try {
+            tx = session.beginTransaction();
+            session.save(doc);
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            session.close();
+        }
     }
 
     /**
@@ -42,11 +43,17 @@ public class DocsWebService {
     @WebMethod(operationName = "updateDocument")
     @Oneway
     public void updateDocument(@WebParam(name = "doc") Document doc) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.merge(doc);
-        em.getTransaction().commit();
-        em.close();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx;
+        try {
+            tx = session.beginTransaction();
+            session.update(doc);
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            session.close();
+        }
     }
 
     /**
@@ -57,12 +64,19 @@ public class DocsWebService {
     @WebMethod(operationName = "removeDocument")
     @Oneway
     public void removeDocument(@WebParam(name = "id") int id) {
-        EntityManager em = emf.createEntityManager();
-        Document doc = (Document) em.createNamedQuery("Document.findById").setParameter("id", id).getSingleResult();
-        em.getTransaction().begin();
-        em.remove(doc);
-        em.getTransaction().commit();
-        em.close();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx;
+        try {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("from Document as doc where doc.id=" + id);
+            Document document = (Document) query.uniqueResult();
+            session.delete(document);
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            session.close();
+        }
     }
 
     /**
@@ -73,9 +87,20 @@ public class DocsWebService {
      */
     @WebMethod(operationName = "getDocument")
     public Document getDocument(@WebParam(name = "id") int id) {
-        EntityManager em = emf.createEntityManager();
-        Document doc = (Document) em.createNamedQuery("Document.findById").setParameter("id", id).getSingleResult();
-        return doc;
+        Document document = new Document();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx;
+        try {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("from Document as doc where doc.id=" + id);
+            document = (Document) query.uniqueResult();
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            session.close();
+        }
+        return document;
     }
 
     /**
@@ -86,9 +111,19 @@ public class DocsWebService {
      */
     @WebMethod(operationName = "getDocuments")
     public List<Document> getDocuments() {
-        EntityManager em = emf.createEntityManager();
-        List<Document> documents = em.createNamedQuery("Document.findAll").getResultList();
+        List<Document> documents = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx;
+        try {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("from Document");
+            documents = query.list();
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            session.close();
+        }
         return documents;
     }
-
 }
--- docsrepo-ws/src/main/java/docsrepo/ws/entities/Document.java
+++ docsrepo-ws/src/main/java/docsrepo/ws/entities/Document.java
@@ -1,76 +1,34 @@
 package docsrepo.ws.entities;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 
-@Entity
-@Table(name = "DOCUMENT")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Document.findAll", query = "SELECT d FROM Document d"),
-    @NamedQuery(name = "Document.findById", query = "SELECT d FROM Document d WHERE d.id = :id"),
-    @NamedQuery(name = "Document.findByName", query = "SELECT d FROM Document d WHERE d.name = :name"),
-    @NamedQuery(name = "Document.findByContentDescription", query = "SELECT d FROM Document d WHERE d.contentDescription = :contentDescription")})
 public class Document implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "ID")
-    private Integer id;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 512)
-    @Column(name = "NAME")
+    private int id;
     private String name;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 2048)
-    @Column(name = "CONTENT_DESCRIPTION")
     private String contentDescription;
-    @Basic(optional = false)
-    @NotNull
-    @Lob
-    @Column(name = "FILE")
     private byte[] file;
 
     public Document() {
     }
 
-    public Document(Integer id) {
-        this.id = id;
-    }
-
-    public Document(Integer id, String name, String contentDescription, byte[] file) {
+    public Document(int id, String name, String contentDescription, byte[] file) {
         this.id = id;
         this.name = name;
         this.contentDescription = contentDescription;
         this.file = file;
     }
 
-    public Integer getId() {
-        return id;
+    public int getId() {
+        return this.id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
@@ -78,7 +36,7 @@ public class Document implements Serializable {
     }
 
     public String getContentDescription() {
-        return contentDescription;
+        return this.contentDescription;
     }
 
     public void setContentDescription(String contentDescription) {
@@ -86,36 +44,11 @@ public class Document implements Serializable {
     }
 
     public byte[] getFile() {
-        return file;
+        return this.file;
     }
 
     public void setFile(byte[] file) {
         this.file = file;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Document)) {
-            return false;
-        }
-        Document other = (Document) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "docsrepo.ws.entities.Document[ id=" + id + " ]";
     }
 
 }
